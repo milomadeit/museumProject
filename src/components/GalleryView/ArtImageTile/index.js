194ce13ab8348e9d679a240c3e653c9b26d7f4c2
Zach Milo <zachmilo@gmail.com>
2023-10-14 10:25:41 -0400

    solve for phase 4

---
+++ src/components/GalleryView/ArtImageTile/index.js
@@ -0,0 +1,16 @@
+import {useParams, Link} from 'react-router-dom';
+
+import './ArtImageTile.css'
+
+function ArtImageTile({art}) {
+    const { galleryId }= useParams();
+
+    return (
+        <Link to={`/galleries/${galleryId}/art/${art.id}`}>
+            <img src={art.images[0].baseimageurl} alt='art' />
+        </Link>
+    );
+};
+
+
+export default ArtImageTile;
--- src/components/GalleryView/index.js
+++ src/components/GalleryView/index.js
@@ -1,16 +1,23 @@
-import {NavLink, useParams} from 'react-router-dom';
+import {useParams, Route } from 'react-router-dom';
+import ArtImageTile from './ArtImageTile';
+
 
 function GalleryView({galleries}) {
+
     let {galleryId} = useParams();
     const galleryView = galleries.find((gallery) => gallery.galleryid === parseInt(galleryId, 10))
-    console.log(galleryView)
-    return (
-        <>
-        <h1>Hello from GalleryView</h1>
-        <h2>{`${galleryView.name}`}</h2>
-        </>
-    )
-}
 
+    return (
+        <div>
+            <h2>{`${galleryView.name}`}</h2>
+            <Route exact path='/galleries/:galleryId'>
+                {galleryView.objects.map(art => (
+                    <ArtImageTile  key={art.id} art ={art}/>
+                ))}
+            </Route>
+        </div>
+
+    );
+};
 
 export default GalleryView;
